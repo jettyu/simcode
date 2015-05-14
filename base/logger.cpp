fb5af052b15243fee11d3d6b67250ac2282743b2
udoyu <xiyingit@gmail.com>
2015-05-14 02:13:17 +0000

    add logger

---
+++ base/logger.cpp
@@ -0,0 +1,55 @@
+#include <simcode/base/logger.h>
+#include <stdio.h>
+using namespace simcode;
+
+int LogLevel::LEVEL_DEBUG = 1;
+int LogLevel::LEVEL_INFO  = 2;
+int LogLevel::LEVEL_WARN  = 3;
+int LogLevel::LEVEL_ERROR = 4;
+int LogLevel::LEVEL_FATAL = 5;
+
+int Logger::level_ = LogLevel::LEVEL_DEBUG;
+Logger::log_func_t Logger::log_func_ = Logger::log_out;
+
+void Logger::set_level(int l)
+{
+    level_ = l;
+}
+
+void Logger::set_log_fun(log_func_t f)
+{
+    log_func_ = f;
+}
+
+void Logger::log_write(int level, 
+                          const char* filename, 
+                          int linenum, 
+                          const char* fmt, ...)
+{
+    if (level < level_) return;
+    va_list ap;
+    va_start(ap, fmt);
+    log_out_valist(level, filename, linenum, fmt, ap);
+    va_end(ap);
+}
+
+#define LOG_BUF_LEN       4096
+void Logger::log_out_valist(int level,
+                        const char* filename,
+                        int linenum,
+                        const char* fmt,
+                        va_list ap)
+{
+    char buf[LOG_BUF_LEN];
+    vsnprintf(buf, LOG_BUF_LEN, fmt, ap);
+    log_func_(level, filename, linenum, buf);
+}
+
+
+void Logger::log_out(int level, 
+                        const char* filename, 
+                        int linenum, 
+                        const char* msg)
+{
+    printf("log_level=%d|%s|%d|%s\n", level, filename, linenum, msg);
+}
---
+++ base/logger.h
@@ -0,0 +1,71 @@
+#ifndef SIMCODE_NET_NETNETLOGGER_H
+#define SIMCODE_NET_NETNETLOGGER_H
+#include <stdarg.h>
+namespace simcode
+{
+
+class LogLevel
+{
+public:
+    static void Init(int debug=2,
+                int info=3,
+                int warn=4,
+                int error=5,
+                int fatal=6)
+    {
+        LEVEL_DEBUG = debug;
+        LEVEL_INFO = info;
+        LEVEL_WARN = warn;
+        LEVEL_ERROR = error;
+        LEVEL_FATAL = fatal;
+    }
+    static int LEVEL_DEBUG;
+    static int LEVEL_INFO;
+    static int LEVEL_WARN;
+    static int LEVEL_ERROR;
+    static int LEVEL_FATAL;
+};
+
+class Logger
+{
+public:
+    //日志级别，文件名，文件行号，日志内容
+    typedef void (*log_func_t)(int level, 
+                               const char* filename, 
+                               int linenum, 
+                               const char* msg);
+    static void set_level(int l);
+    static void set_log_fun(log_func_t f);
+    static void log_write(int level, 
+                         const char* filename, 
+                         int linenum, 
+                         const char *fmt, ...);
+private:
+    static void log_out(int level, 
+                        const char* filename, 
+                        int linenum, 
+                        const char* msg);
+    static void log_out_valist(int level, 
+                               const char* filename, 
+                               int linenum, 
+                               const char*fmt, 
+                               va_list ap);
+private:
+    static int level_;
+    static log_func_t log_func_;
+};
+
+}
+
+#define NETLOG_DEBUG(fmt, args...)    \
+        simcode::Logger::log_write(simcode::LogLevel::LEVEL_DEBUG, __FILE__, __LINE__, fmt, ##args)
+#define NETLOG_INFO(fmt, args...)    \
+        simcode::Logger::log_write(simcode::LogLevel::LEVEL_INFO,  __FILE__, __LINE__, fmt, ##args)
+#define NETLOG_WARN(fmt, args...)    \
+        simcode::Logger::log_write(simcode::LogLevel::LEVEL_WARN,  __FILE__, __LINE__, fmt, ##args)
+#define NETLOG_ERROR(fmt, args...)    \
+        simcode::Logger::log_write(simcode::LogLevel::LEVEL_ERROR, __FILE__, __LINE__, fmt, ##args)
+#define NETLOG_FATAL(fmt, args...)    \
+        simcode::Logger::log_write(simcode::LogLevel::LEVEL_FATAL, __FILE__, __LINE__, fmt, ##args
+
+#endif
