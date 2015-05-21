61d3180af4c4021578732c51e18e9115a658b89a
xyit <xiyingit@gmail.com>
2015-05-21 15:07:34 +0800

    mod filter + to append

--- mysql/filter_template.h
+++ mysql/filter_template.h
@@ -61,7 +61,7 @@ public:
             const std::string& logic=" and ");
     OBJ& In(const std::string& name, 
             const std::vector<uint64_t>& value,
-            const std::string& logic=" and ")
+            const std::string& logic=" and ");
     OBJ& Between(const std::string& name,
                  uint64_t value1, 
                  uint64_t value2,
@@ -98,8 +98,8 @@ inline OBJ& SqlFilter<OBJ>::Filter(const std::string& name,
                                    const std::string& logic,
                                    const std::string& symbol)
 {
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " " + symbol + " '" + escape_(value)  + "'";
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" ").append(symbol).append(" '").append(escape_(value)).append("'");
     flag_ = true;
     return obj_;
 }
@@ -110,8 +110,8 @@ inline OBJ& SqlFilter<OBJ>::Filter(const std::string& name,
                                    const std::string& logic,
                                    const std::string& symbol)
 {
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " " + symbol + " " + AtoStr(value);
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" ").append(symbol).append(" ").append(AtoStr(value));
     flag_ = true;
     return obj_;
 }
@@ -122,8 +122,8 @@ inline OBJ& SqlFilter<OBJ>::FilterNest(const std::string& name,
                                    const std::string& logic,
                                    const std::string& symbol)
 {
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " " + symbol + " " + value;
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" ").append(symbol).append(" ").append(value);
     flag_ = true;
     return obj_;
 }
@@ -134,8 +134,8 @@ OBJ& SqlFilter<OBJ>::Filter(const OBJ& f,
                             const std::string& left_symbol,
                             const std::string& right_symbol)
 {
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += left_symbol + f.ToStr() + right_symbol;
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(left_symbol).append(f.ToStr()).append(right_symbol);
     flag_ = true;
     return obj_;
 }
@@ -143,35 +143,35 @@ OBJ& SqlFilter<OBJ>::Filter(const OBJ& f,
 template<class OBJ>
 OBJ& SqlFilter<OBJ>::Filter(const std::string& str)
 {
-    filter_ += str;
+    filter_.append(str);
     return obj_;
 }
 
 template<class OBJ>
 OBJ& SqlFilter<OBJ>::OrderBy(const std::string& name)
 {
-    filter_ += " order by " + name;
+    filter_.append(" order by ").append(name);
     return obj_;
 }
 
 template<class OBJ>
 OBJ& SqlFilter<OBJ>::OrderByDesc(const std::string& name)
 {
-    filter_ += " order by " + name + " desc";
+    filter_.append(" order by ").append(name).append(" desc");
     return obj_;
 }
 
 template<class OBJ>
 OBJ& SqlFilter<OBJ>::Limit(size_t i)
 {
-    filter_ += " limit " + AtoStr(i);
+    filter_.append(" limit ").append(AtoStr(i));
     return obj_;
 }
 
 template<class OBJ>
 OBJ& SqlFilter<OBJ>::Limit(size_t i, size_t j)
 {
-    filter_ += " limit " + AtoStr(i) + "," + AtoStr(j);
+    filter_.append(" limit ").append(AtoStr(i)).append(",").append(AtoStr(j));
     return obj_;
 }
 
@@ -181,13 +181,13 @@ OBJ& SqlFilter<OBJ>::In(const std::string& name,
                         const std::string& logic)
 {
     if (value.empty()) return obj_;
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " in (";
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" in (");
     std::vector<std::string>::const_iterator it;
     for (it=value.begin(); it!=value.end(); ++it)
-        filter_ += "'" + escape_(*it) + "',";
+        filter_.append("'").append(escape_(*it)).append("',");
     filter_.erase(filter_.size()-1);
-    filter_ += ")";
+    filter_.append(")");
     return obj_;
 }
 
@@ -197,13 +197,13 @@ OBJ& SqlFilter<OBJ>::In(const std::string& name,
                         const std::string& logic)
 {
     if (value.empty()) return obj_;
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " in (";
-    std::vector<std::string>::const_iterator it;
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" in (");
+    std::vector<std::uint64_t>::const_iterator it;
     for (it=value.begin(); it!=value.end(); ++it)
-        filter_ += AtoStr(*it) + ",";
+        filter_.append(AtoStr(*it)).append(",");
     filter_.erase(filter_.size()-1);
-    filter_ += ")";
+    filter_.append(")");
     return obj_;
 }
 
@@ -211,10 +211,10 @@ template<class OBJ>
 OBJ& SqlFilter<OBJ>::Between(const std::string& name,
                              uint64_t value1, 
                              uint64_t value2,
-                             const std::string& logic=" and ")
+                             const std::string& logic)
 {
-    if (flag_) filter_ += " " + logic + " ";
-    filter_ += name + " between (" + AtoStr<value1> + "," + AtoStr<value2> + ")";
+    if (flag_) filter_.append(" ").append(logic).append(" ");
+    filter_.append(name).append(" between (").append(AtoStr(value1)).append(",").append(AtoStr(value2)).append(")");
     return obj_;
 }
 
