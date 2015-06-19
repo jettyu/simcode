125654e5570ca54d0920b69df42fe8611aa02bc3
xyit <xiyingit@gmail.com>
2015-06-19 22:06:39 +0800

    add log

--- log/logging.h
+++ log/logging.h
@@ -1,25 +1,30 @@
 #ifndef SIMCODE_LOGGER_LOGGER_H
 #define SIMCODE_LOGGER_LOGGER_H
-#include <simcode/logger/logfile.h>
+#include <simcode/log/logfile.h>
 #include <simcode/base/typedef.h>
 #include <simcode/base/logger.h>
 namespace simcode
 {
-namespace logger
+namespace log
 {
 class Logging : public simcode::BaseLogger
 {
 public:
-    int Init(const std::string& filename, int level, uint64_t rotate_size);
+    int Init(const std::string& filename, uint64_t rotate_size);
     void log_out(int level,
                  const char* filename,
                  int linenum,
                  const char* funcname,
                  const char* msg);
 private:
-    simcode::Logger logger_;
     LogFile logFile_;
     Mutex mutex_;
+};
+
+class GlobalLogging
+{
+public:
+    int Init(const std::string& name, uint64_t rotate_size, int level);
 };
 
 }
