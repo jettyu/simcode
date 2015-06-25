cedd576369f6d245b02e8047ed06fe18b64a3edd
jettyu <xiyingit@gmail.com>
2015-06-25 12:40:05 +0800

    add example

---
+++ net/example/tcp/client/client.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <simcode/net/Socket.h>
+#include <simcode/net/SockAddr.h>
+#include <simcode/net/EPollPoller.h>
+#include <simcode/net/EventLoopThread.h>
+#include <simcode/net/TcpClient.h>
+#include <iostream>
+using namespace std;
+using namespace simcode;
+using namespace simcode::net;
+
+//#include "oci_api.h"
+
+struct Head
+{
+    uint16_t size;
+    void ParseFromPointer(const void* data)
+    {
+        *this = *static_cast<const Head*>(data);
+        size = ntohs(size);
+    }
+    void SerializeToPointer(void* data)
+    {
+        size = htons(size);
+        memcpy(data, this, LENGTH);
+    }
+    static const size_t LENGTH;
+};
+
+const size_t Head::LENGTH = sizeof(Head);
+
+void eventHandle(const TcpConnectionPtr& conn, const void* msg, int size)
+{
+    cout<<"recv:"<<std::string((const char*)msg+Head::LENGTH, size - Head::LENGTH)<<endl;
+}
+
+void codec(const TcpConnectionPtr& conn, simcode::net::Buffer *msg)
+{
+    while (msg->readableBytes() >= Head::LENGTH)
+    {
+        Head head;
+        head.ParseFromPointer(msg->peek());
+        int totle_len = head.size + Head::LENGTH;
+        if (msg->readableBytes() < totle_len) break;
+        //cout<<"msgLen="<<totle_len<<endl;
+        eventHandle(conn, msg->peek(), totle_len);
+        msg->seek(totle_len);
+    }
+    msg->retrieve(msg->getSeek());
+}
+
+void onConnection(const TcpConnectionPtr& conn)
+{
+    cout<<"connection success!"<<endl;
+    //conn->send("conn success", 12);
+}
+
+void onClose(const TcpConnectionPtr&)
+{
+    cout<<"closed ..."<<endl;
+}
+
+int main(int argc, char **argv)
+{
+    EventLoopThread loop;
+    Socket sock(AF_INET, SOCK_STREAM, 0);
+    SockAddr peerAddr("127.0.0.1", 10010);
+    TcpClient client(loop.startLoop(), peerAddr, "tcpclient");
+    client.setMessageCallback(simex::bind(codec, _1, _2));
+    client.setConnectionCallback(simex::bind(onConnection, _1));
+    client.setCloseCallback(simex::bind(onClose, _1));
+    client.active();
+    std::string inputStr;
+    std::string sendStr;
+    while (1)
+    {
+        inputStr.clear();
+        cin >> inputStr;
+        if (inputStr == "exit") return 0;
+        Head head;
+        head.size = inputStr.size();
+        sendStr.resize(Head::LENGTH);
+        head.SerializeToPointer(&sendStr[0]);
+        sendStr.append(inputStr);
+        client.send(sendStr);
+    }
+    return 0;
+}
---
+++ net/example/tcp/simple_server/server.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <simcode/net/Socket.h>
+#include <simcode/net/SockAddr.h>
+#include <simcode/net/EPollPoller.h>
+#include <simcode/net/EventLoop.h>
+#include <simcode/net/Acceptor.h>
+#include <simcode/net/TcpConnection.h>
+#include <simcode/net/TcpServer.h>
+#include <iostream>
+using namespace std;
+using namespace simcode;
+using namespace simcode::net;
+
+void eventHandle(const TcpConnectionPtr& conn, const char* data, int size)
+{
+    cout<<string(data+2, size-2)<<endl;
+    conn->send(data, size);
+}
+
+struct Head
+{
+    uint16_t size;
+    void ParseFromPointer(const void* data)
+    {
+        *this = *static_cast<const Head*>(data);
+        size = ntohs(size);
+    }
+    void SerializeToPointer(void* data)
+    {
+        size = htons(size);
+        memcpy(data, this, LENGTH);
+    }
+    static const size_t LENGTH;
+};
+
+const size_t Head::LENGTH = sizeof(Head);
+
+void codec(const TcpConnectionPtr& conn, simcode::net::Buffer *msg)
+{
+    while (msg->readableBytes() >= Head::LENGTH)
+    {
+        Head head;
+        head.ParseFromPointer(msg->peek());
+        int totle_len = head.size + Head::LENGTH;
+        cout<<"msgLen="<<totle_len<<endl;
+        if (msg->readableBytes() < totle_len) break;
+
+        eventHandle(conn, (const char*)msg->peek(), totle_len);
+        msg->seek(totle_len);
+    }
+    msg->retrieve(msg->getSeek());
+}
+
+//#include "oci_api.h"
+
+int main(int argc, char **argv)
+{
+    signal(SIGPIPE, SIG_IGN);
+    EventLoop loop;
+    SockAddr listenAddr("0.0.0.0", 10010);
+    TcpServer server(&loop, listenAddr, "tcpsvr");
+    server.setMessageCallback(simex::bind(codec, _1, _2));
+    server.start();
+    loop.loop();
+    return 0;
+}
