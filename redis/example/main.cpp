0f42bfe2af3931c1a6c5f46426f6f032d8bbe855
jettyu <xiyingit@gmail.com>
2015-07-10 14:35:07 +0800

    add example

---
+++ redis/example/main.cpp
@@ -0,0 +1,79 @@
+#include <simcode/redis/async_redis.h>
+#include <unistd.h>
+
+#define TEST_LIBEVENT 0 //if test libevent, please change 0 to 1
+
+#if TEST_LIBEVENT
+	#include <hiredis/adapters/libevent.h>
+#else
+	#include <simcode/redis/libsimcode.h>
+#endif
+using namespace simcode;
+using namespace redis;
+using namespace std;
+
+static void commandCallback(AsyncRedis* ar, redisReply* rp)
+{
+        cout<<"get:"<<rp->str<<endl;
+}
+
+#if TEST_LIBEVENT
+
+static void Attach(struct event_base* base, redisAsyncContext* c)
+{
+        redisLibeventAttach(c, base);
+}
+
+
+static void test_libevent()
+{
+    AsyncRedis ar;
+    RedisInfo info;
+    struct event_base *base = event_base_new();
+    info.host = "127.0.0.1";
+    info.port = 6379;
+    info.time_out={1, 1500};
+    ar.set_redisinfo(info);
+    ar.set_attachCallback(simex::bind(Attach, base, _1));
+    ar.Connect();
+    sleep(1);
+    ar.Command(NULL, "SET %s %s", "async", "test");
+    ar.Command(simex::bind(commandCallback, _1, _2), "GET %s", "async");
+    event_base_dispatch(base);
+}
+
+#else
+
+static void SimcodeAttach(net::EventLoop* loop, redisAsyncContext* c)
+{
+        redisLibSimcodeAttach(c, loop);
+}
+
+static void test_libsimcode()
+{
+    AsyncRedis ar;
+    RedisInfo info;
+    info.host = "127.0.0.1";
+    info.port = 6379;
+    info.time_out={1, 1500};
+    ar.set_redisinfo(info);
+    net::EventLoop loop;
+    ar.set_attachCallback(simex::bind(SimcodeAttach, &loop, _1));
+    ar.Connect();
+    sleep(1);
+    ar.Command(NULL, "SET %s %s", "async", "test");
+    ar.Command(simex::bind(commandCallback, _1, _2), "GET %s", "async");
+    loop.loop();
+}
+
+#endif
+
+int main()
+{
+#if TEST_LIBEVENT
+    test_libevent();
+#else
+    test_libsimcode();
+#endif
+    return 0;
+}
